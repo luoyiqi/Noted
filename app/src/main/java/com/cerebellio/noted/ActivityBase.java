04a8d77b5c3dd71fbaf54d0ea4350d85b153e12b
Sam <sh893@bath.ac.uk>
2016-02-12 00:52:06 +0000

    Added version check and made status bar translucent

--- app/src/main/java/com/cerebellio/noted/ActivityBase.java
+++ app/src/main/java/com/cerebellio/noted/ActivityBase.java
@@ -1,8 +1,11 @@
 package com.cerebellio.noted;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import com.cerebellio.noted.utils.Constants;
 
@@ -11,6 +14,8 @@ import com.cerebellio.noted.utils.Constants;
  */
 public class ActivityBase extends AppCompatActivity {
 
+    protected final int TRANSITION_VERTICAL = 0;
+    protected final int TRANSITION_HORIZONTAL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,17 @@ public class ActivityBase extends AppCompatActivity {
                 .getInt(Constants.SHARED_PREFS_THEME_ID, Constants.DEFAULT_THEME_ID);
         setTheme(themeId);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
         super.onCreate(savedInstanceState);
+    }
+
+    protected void animateFragmentTransition(FragmentTransaction transaction, int direction) {
+        if (direction == TRANSITION_HORIZONTAL) {
+            transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left,
+                    R.anim.slide_in_left, R.anim.slide_out_right);
+        }
     }
 }
