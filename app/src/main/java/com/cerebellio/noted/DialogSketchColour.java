44c7d0f6599b7e377d61f8291ad138d5267fe413
Sam <sh893@bath.ac.uk>
2016-02-23 12:44:46 +0000

    Passed colour alpha responsibility elsewhere

--- app/src/main/java/com/cerebellio/noted/DialogSketchColour.java
+++ app/src/main/java/com/cerebellio/noted/DialogSketchColour.java
@@ -9,24 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.SeekBar;
 
-import com.cerebellio.noted.models.listeners.IOnAlphaChangedListener;
 import com.cerebellio.noted.models.listeners.IOnColourSelectedListener;
 import com.cerebellio.noted.utils.Constants;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 /**
  * Created by Sam on 12/02/2016.
  */
 public class DialogSketchColour extends DialogFragment implements IOnColourSelectedListener {
 
-    @InjectView(R.id.popup_stroke_colour_selection_alpha_seekbar) SeekBar mSeekbarAlpha;
-
     private IOnColourSelectedListener mIOnColourSelectedListener;
-    private IOnAlphaChangedListener mIOnAlphaChangedListener;
     private FragmentColourSelection mFragmentColourSelection;
 
     public DialogSketchColour() {}
@@ -37,8 +29,7 @@ public class DialogSketchColour extends DialogFragment implements IOnColourSelec
 
         getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        View rootView = getActivity().getLayoutInflater().inflate(R.layout.popup_colour_selection, null);
-        ButterKnife.inject(this, rootView);
+        View rootView = getActivity().getLayoutInflater().inflate(R.layout.dialog_colour_selection, null);
 
         mFragmentColourSelection = new FragmentColourSelection();
         Bundle bundle = new Bundle();
@@ -47,28 +38,8 @@ public class DialogSketchColour extends DialogFragment implements IOnColourSelec
 
         getChildFragmentManager()
                 .beginTransaction()
-                .replace(R.id.popup_stroke_colour_selection, mFragmentColourSelection)
+                .replace(R.id.dialog_stroke_colour_selection, mFragmentColourSelection)
                 .commit();
-
-        mSeekbarAlpha.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                mFragmentColourSelection.alphaChanged(progress);
-                mIOnAlphaChangedListener.onAlphaChanged(progress);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        mSeekbarAlpha.setProgress(getArguments().getInt(Constants.BUNDLE_CURRENT_COLOUR_ALPHA, 255));
 
         return rootView;
     }
@@ -78,7 +49,6 @@ public class DialogSketchColour extends DialogFragment implements IOnColourSelec
         super.onAttach(context);
         try {
             mIOnColourSelectedListener = (IOnColourSelectedListener) getParentFragment();
-            mIOnAlphaChangedListener = (IOnAlphaChangedListener) getParentFragment();
         } catch (ClassCastException e) {
             Log.e("Listener failure", "Calling context must implement listeners");
         }
