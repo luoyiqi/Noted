bfb2d5c58a772aaeb08a17fbee2b94d843501486
Sam <sh893@bath.ac.uk>
2016-02-23 12:57:25 +0000

    Generalised to Item

--- app/src/main/java/com/cerebellio/noted/models/listeners/IOnFloatingActionMenuOptionClickedListener.java
+++ app/src/main/java/com/cerebellio/noted/models/listeners/IOnFloatingActionMenuOptionClickedListener.java
@@ -1,9 +1,10 @@
 package com.cerebellio.noted.models.listeners;
 
+import com.cerebellio.noted.models.Item;
+
 /**
  * Created by Sam on 09/02/2016.
  */
 public interface IOnFloatingActionMenuOptionClickedListener {
-    void OnFabNewNoteClick();
-    void OnFabNewChecklistClick();
+    void OnFabCreateItemClick(Item.Type type);
 }
