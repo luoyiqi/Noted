5cfa8a48493429f247236fae7b6137175fe67b60
Sam <sh893@bath.ac.uk>
2016-02-23 12:56:45 +0000

    Initial commit

---
+++ app/src/main/java/com/cerebellio/noted/models/listeners/IOnSketchActionListener.java
@@ -0,0 +1,8 @@
+package com.cerebellio.noted.models.listeners;
+
+/**
+ * Created by Sam on 13/02/2016.
+ */
+public interface IOnSketchActionListener {
+    void onChange();
+}
---
+++ app/src/main/java/com/cerebellio/noted/models/listeners/IOnStrokeWidthChangedListener.java
@@ -0,0 +1,8 @@
+package com.cerebellio.noted.models.listeners;
+
+/**
+ * Created by Sam on 12/02/2016.
+ */
+public interface IOnStrokeWidthChangedListener {
+    void onStrokeWidthChanged(int width);
+}
