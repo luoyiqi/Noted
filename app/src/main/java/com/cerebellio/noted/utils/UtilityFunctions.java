0d1a2490cc08e3ca05be9373dcbf3e1f85da1790
Sam <sh893@bath.ac.uk>
2016-02-23 22:07:29 +0000

    Added Bitmap save/retrieval methods

--- app/src/main/java/com/cerebellio/noted/utils/UtilityFunctions.java
+++ app/src/main/java/com/cerebellio/noted/utils/UtilityFunctions.java
@@ -1,20 +1,29 @@
 package com.cerebellio.noted.utils;
 
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.text.format.DateFormat;
 import android.util.TypedValue;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Random;
 
 /**
- * Created by Sam on 09/02/2016.
+ * Contains a number of functions used throughout the app
  */
 public abstract class UtilityFunctions {
 
@@ -71,10 +80,45 @@ public abstract class UtilityFunctions {
     /**
      * Grabs a random Integer from a given array
      *
-     * @return              randomly selected Integer
+     * @return randomly selected Integer
      */
     public static Integer getRandomIntegerFromArray(Integer[] fullList) {
         return fullList[new Random().nextInt(fullList.length)];
     }
 
+    public static Bitmap getBitmapFromFile(String path) {
+        Bitmap bitmap = null;
+
+        try {
+            File f = new File(path);
+            bitmap = BitmapFactory.decodeStream(new FileInputStream(f));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
+
+    public static String saveSketchToStorage(Bitmap sketch, Context context) throws IOException {
+        ContextWrapper contextWrapper = new ContextWrapper(context.getApplicationContext());
+        File directory = contextWrapper.getDir("sketches", Context.MODE_PRIVATE);
+        String  fileName = DateFormat.format("MM-dd-yy HH-mm-ss", new Date().getTime()).toString();
+        File path = new File(directory, fileName);
+
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(path);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            sketch.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                fos.close();
+            }
+        }
+
+        return path.getAbsolutePath();
+    }
 }
