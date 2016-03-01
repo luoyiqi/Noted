d8c87a353acf4be9c8a08bfcccd23a2a4ee99c78
Sam <sh893@bath.ac.uk>
2016-03-01 16:55:06 +0000

    Removed unused vertical transition

--- app/src/main/java/com/cerebellio/noted/ActivityBase.java
+++ app/src/main/java/com/cerebellio/noted/ActivityBase.java
@@ -1,12 +1,11 @@
 package com.cerebellio.noted;
 
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
-import com.cerebellio.noted.utils.Constants;
+import com.cerebellio.noted.utils.PreferenceFunctions;
 import com.cerebellio.noted.utils.TextFunctions;
 
 /**
@@ -16,15 +15,16 @@ public abstract class ActivityBase extends AppCompatActivity {
 
     private static final String LOG_TAG = TextFunctions.makeLogTag(ActivityBase.class);
 
-    protected final int TRANSITION_VERTICAL = 0;
     protected final int TRANSITION_HORIZONTAL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        int themeId = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
-                .getInt(Constants.SHARED_PREFS_THEME_ID, Constants.DEFAULT_THEME_ID);
-        setTheme(themeId);
+        if (PreferenceFunctions.getPrefTheme(getApplicationContext())
+                .equals(PreferenceFunctions.SETTINGS_DISPLAY_THEME_LIGHT)) {
+            setTheme(R.style.NotedTheme_Default);
+        } else {
+            setTheme(R.style.NotedTheme_Dark);
+        }
 
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 //            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -36,14 +36,12 @@ public abstract class ActivityBase extends AppCompatActivity {
     /**
      * Animates a fragment transition
      * @param transaction   {@link FragmentTransaction} to animate
-     * @param direction     {@link #TRANSITION_HORIZONTAL} or {@link #TRANSITION_VERTICAL}
+     * @param direction     {@link #TRANSITION_HORIZONTAL}
      */
     protected void animateFragmentTransition(FragmentTransaction transaction, int direction) {
         if (direction == TRANSITION_HORIZONTAL) {
             transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left,
                     R.anim.slide_in_left, R.anim.slide_out_right);
-        } else if (direction == TRANSITION_VERTICAL) {
-            //TODO animate vertical transition
         }
     }
 
