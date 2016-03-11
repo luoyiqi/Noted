660d9f17914c2c0644c4510c078dbaddbf646afb
Sam <sh893@bath.ac.uk>
2016-03-11 16:19:01 +0000

    Sets default preferences

--- app/src/main/java/com/cerebellio/noted/ApplicationNoted.java
+++ app/src/main/java/com/cerebellio/noted/ApplicationNoted.java
@@ -7,6 +7,8 @@ import com.cerebellio.noted.utils.TextFunctions;
 import com.squareup.otto.Bus;
 import com.squareup.otto.ThreadEnforcer;
 
+import java.util.Random;
+
 /**
  * Application extension for this app
  */
@@ -15,12 +17,15 @@ public class ApplicationNoted extends Application {
     private static final String LOG_TAG = TextFunctions.makeLogTag(ApplicationNoted.class);
 
     public static Bus bus = new Bus(ThreadEnforcer.MAIN);
+    public static Random random = new Random();
 
     @Override
     public void onCreate() {
         super.onCreate();
         PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.settings_display, false);
         PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.settings_wordcloud, false);
+        PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.settings_behaviour, false);
         PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.settings_feedback, false);
     }
+
 }
